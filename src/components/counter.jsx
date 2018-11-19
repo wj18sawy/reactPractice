d061672022b6c630bb8b225e77cc2cf081be94f6
Wyatt Sawyer <wj18sawy@alum.siena.edu>
2018-11-18 20:18:50 -0500

    Worked on buttons, changing styles, attributes, etc. in counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
   render() {
-    return <h1>Hello World1</h1>;
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment Button</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state; //this.state.count
+    return count === 0 ? "Zero" : count;
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 }
 
